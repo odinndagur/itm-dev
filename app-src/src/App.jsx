121a1f6c723cdc243445db7300004c42b412d53c
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-03-22 21:14:07 +0000

    deploy

--- app-src/src/App.jsx
+++ app-src/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [searchValue, setSearchValue] = useState('')
+  const [signs, setSigns] = useState([])
+
+  useEffect(() => {
+    window.promiseWorker.postMessage({
+      type:'searchValue',
+      searchValue:searchValue
+    }).then(searchResults => {
+      setSigns(searchResults)
+    })
+  },[searchValue])
 
   return (
     <div className="App">
-      <h1>lolz</h1>
-        
+      <header>
+        <h1>lolz</h1>
+        <input value={searchValue} onChange={e => setSearchValue(e.target.value)}/>
+      </header>
+      {signs.map(sign => {
+        return <div key={sign.id}>{sign.phrase}</div>
+      })}
     </div>
   )
 }
