502f766a6054b59aced4b944a6c9ce0bbc6b6b8c
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-03-23 00:39:10 +0000

    deploy

--- app-src/vite.config.js
+++ app-src/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/itm-dev/',
+  base: '/itm-dev/',
   plugins: [react()],
   // build: {
   //   watch: {
