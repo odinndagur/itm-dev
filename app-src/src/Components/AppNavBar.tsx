df256bab96e0e1371d4e5dcb485dab5a0ef5d271
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-04-12 21:02:03 +0000

    deploy

--- app-src/src/Components/AppNavBar.tsx
+++ app-src/src/Components/AppNavBar.tsx
@@ -1,15 +1,55 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import './AppNavBar.css'
 
 export function AppNavBar() {
     return (
         <footer className="footer">
             <nav>
-                <Link className="nav-item" to="/"><span className="material-symbols-outlined">home</span><span className="nav-text">Heim</span></Link>
-                <Link className="nav-item" to="/signs"><span className="material-symbols-outlined">sign_language</span><span className="nav-text">Öll tákn</span></Link>
-                <Link className="nav-item" to="/leit"><span className="material-symbols-outlined">search</span><span className="nav-text">Leit</span></Link>
-                <Link className="nav-item" to="/collections"><span className="material-symbols-outlined">list</span><span className="nav-text">Táknasöfn</span></Link>
-                <Link className="nav-item" to="/settings"><span className="material-symbols-outlined">account_box</span><span className="nav-text">Stillingar</span></Link>
+                <NavLink className="nav-item" to="/" style={({ isActive, isPending }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+    };
+  }}>
+                    <div className="material-symbols-outlined">home</div>
+                    <div className="nav-text">Heim</div>
+                </NavLink>
+                <NavLink className="nav-item" to="/signs" style={({ isActive, isPending }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+    };
+  }}>
+                    <div className="material-symbols-outlined">sign_language</div>
+                    <div className="nav-text">Öll tákn</div>
+                </NavLink>
+                <NavLink className="nav-item" to="/leit" style={({ isActive, isPending }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+    };
+  }}>
+                    <div className="material-symbols-outlined">search</div>
+                    <div className="nav-text">Leit</div>
+                </NavLink>
+                <NavLink className="nav-item" to="/collections" style={({ isActive, isPending }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+    };
+  }}>
+                    <div className="material-symbols-outlined">list</div>
+                    <div className="nav-text">Táknasöfn</div>
+                </NavLink>
+                <NavLink className="nav-item" to="/settings" style={({ isActive, isPending }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+    };
+  }}>
+                    <div className="material-symbols-outlined">account_box</div>
+                    <div className="nav-text">Stillingar</div>
+                </NavLink>
             </nav>
         </footer>
     )
