8748f33b8285bbe1fd5aeede5b4dd74ec1174abd
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-04-24 19:29:34 +0000

    deploy

--- app-src/src/Components/YoutubeEmbed.tsx
+++ app-src/src/Components/YoutubeEmbed.tsx
@@ -1,14 +1,16 @@
 import './YoutubeEmbed.css'
 export const YoutubeEmbed = ({ embedId }: { embedId: string }) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}?mute=1`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
+    <div className="video-container" style={{ width: '100%', margin: 'auto' }}>
+        <div className="video-responsive">
+            <iframe
+                width="853"
+                height="480"
+                src={`https://www.youtube.com/embed/${embedId}?mute=1`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+            />
+        </div>
+    </div>
 )
