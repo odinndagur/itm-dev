d92fc592d0192ce807d8fb79d7286a9bb57c89e5
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-04-25 14:17:48 +0000

    deploy

--- app-src/src/Components/YoutubeEmbed.tsx
+++ app-src/src/Components/YoutubeEmbed.tsx
@@ -16,7 +16,7 @@ export const YoutubeEmbed = ({ embedId }: { embedId: string }) => {
                 <iframe
                     width="853"
                     height="480"
-                    src={`http://www.youtube.com/embed/${embedId}?mute=1&rel=0&loop=1&playlist=${embedId}&controls=0&playsinline=0&modestbranding=1&origin=https://odinndagur.github.io/itm-dev/`}
+                    src={`https://www.youtube.com/embed/${embedId}?mute=1&rel=0&loop=1&playlist=${embedId}&controls=0&playsinline=0&modestbranding=1&origin=https://odinndagur.github.io/itm-dev/`}
                     frameBorder="0"
                     allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
                     allowFullScreen
