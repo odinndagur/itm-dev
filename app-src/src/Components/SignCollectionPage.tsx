eab44d6145eccff09a11a6a3c38d011d95326058
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-05-05 13:44:13 +0000

    deploy

--- app-src/src/Components/SignCollectionPage.tsx
+++ app-src/src/Components/SignCollectionPage.tsx
@@ -124,6 +124,7 @@ export function SignCollectionPage() {
                 collectionId: search.id,
                 page: page,
             }),
+        staleTime: 0,
         keepPreviousData: true,
     })
 
