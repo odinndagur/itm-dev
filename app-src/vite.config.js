b6f7e7f2d20b5880754af2ee7dbbf2cf4eec9579
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-05-09 11:45:59 +0000

    deploy

--- app-src/vite.config.js
+++ app-src/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,6 +8,46 @@ export default defineConfig({
     plugins: [
         react({
             include: '**/*.tsx',
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
+                navigateFallback: 'index.html',
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'google-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'gstatic-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                ],
+            },
         }),
     ],
 })
